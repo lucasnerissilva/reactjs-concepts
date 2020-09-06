acc49f8ff6d2bcc0b83d13089abf63f88370bb11
Lucas Neris <lucas.neris@globalpoints.com.br>
2020-09-06 19:16:35 -0300

    list repositories, create repository and delete repository was created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import {
+  findRepositories,
+  addRepository,
+  deleteRepository
+} from "./services/api";
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    loadRepositories().then(respositories => {
+      setRepositories(respositories);
+    });
+  }, []);
+
+  const loadRepositories = async () => {
+    const response = await findRepositories("repositories");
+
+    if (!response || !response.data || !response.data.length) return [];
+
+    return response.data;
+  };
+
   async function handleAddRepository() {
     // TODO
+    const response = await addRepository({
+      title: "Gaby delicia",
+      url: "new repository url",
+      techs: ["react", "react-native"]
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await deleteRepository(id);
+    setRepositories(repositories.filter(r => r.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories &&
+          repositories.map(p => {
+            return (
+              <li key={p.id}>
+                {p.title}
+                <button onClick={() => handleRemoveRepository(p.id)}>
+                  Remover
+                </button>
+              </li>
+            );
+          })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://localhost:3333"
 });
+
+export const findRepositories = async () => {
+  return api.get("repositories");
+};
+
+export const addRepository = async repository => {
+  return api.post("repositories", repository);
+};
+
+export const deleteRepository = async id => {
+  return api.delete(`repositories/${id}`);
+};
 
 export default api;
